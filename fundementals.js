578b3094dce586ce7a7e6535dd28a1105bf01803
MeRoty <amira95ibrahim@gmail.com>
2025-11-10 09:20:44 +0200

    Enhance fundementals.js with comments and examples
    Added comments and examples for JavaScript fundamentals.

---
+++ fundementals.js
@@ -0,0 +1,17 @@
+//where to write js
+//order is important
+document.onload();
+//Window Object
+//Document Object
+//Console Object
+//datatypes : string /boolean /int /
+document.write(typeof 5);
+let x=5;
+x=6; //error
+var y=5;
+y='f'; // correct useful in hosting
+const cons='nk';
+
+
+
+  
